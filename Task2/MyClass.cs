16c2267666ea0836871f72d3cc03cea9562020c6
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-29 11:13:11 +0300

    task2

---
+++ Task2/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class MyClass
+    {
+        [Obsolete("This method is obsolete. Call NewMethod1 instead.", false)]
+        public string OldMethod1()
+        {
+            return "Old Method 1";
+        }
+        public string NewMethod1()
+        {
+            return "New Method 1";
+        }
+        [Obsolete("This method is obsolete. Call NewMethod2 instead.", true)]
+        public string OldMethod2()
+        {
+            return "Old Method 2";
+        }
+        public string NewMethod2()
+        {
+            return"New Method 2";
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            Console.WriteLine($"Call OldMethod1: {myClass.OldMethod1()}");
+            Console.WriteLine($"Call OldMethod2: {myClass.OldMethod2()}");
+        }
+    }
+}
