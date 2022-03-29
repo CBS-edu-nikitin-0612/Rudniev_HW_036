da10f88034cf0422c06d5b937b25c55fbd72932e
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-29 13:05:34 +0300

    fulfilled Task3

---
+++ Task3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxMethods = new System.Windows.Forms.CheckBox();
+            this.checkBoxFields = new System.Windows.Forms.CheckBox();
+            this.checkBoxProperties = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 47);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(776, 391);
+            this.textBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "show:";
+            // 
+            // checkBoxMethods
+            // 
+            this.checkBoxMethods.AutoSize = true;
+            this.checkBoxMethods.Location = new System.Drawing.Point(56, 22);
+            this.checkBoxMethods.Name = "checkBoxMethods";
+            this.checkBoxMethods.Size = new System.Drawing.Size(73, 19);
+            this.checkBoxMethods.TabIndex = 3;
+            this.checkBoxMethods.Text = "methods";
+            this.checkBoxMethods.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFields
+            // 
+            this.checkBoxFields.AutoSize = true;
+            this.checkBoxFields.Location = new System.Drawing.Point(135, 22);
+            this.checkBoxFields.Name = "checkBoxFields";
+            this.checkBoxFields.Size = new System.Drawing.Size(54, 19);
+            this.checkBoxFields.TabIndex = 4;
+            this.checkBoxFields.Text = "fields";
+            this.checkBoxFields.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxProperties
+            // 
+            this.checkBoxProperties.AutoSize = true;
+            this.checkBoxProperties.Location = new System.Drawing.Point(195, 22);
+            this.checkBoxProperties.Name = "checkBoxProperties";
+            this.checkBoxProperties.Size = new System.Drawing.Size(79, 19);
+            this.checkBoxProperties.TabIndex = 5;
+            this.checkBoxProperties.Text = "properties";
+            this.checkBoxProperties.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBoxProperties);
+            this.Controls.Add(this.checkBoxFields);
+            this.Controls.Add(this.checkBoxMethods);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxMethods;
+        private System.Windows.Forms.CheckBox checkBoxFields;
+        private System.Windows.Forms.CheckBox checkBoxProperties;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        Assembly assembly = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "";
+            
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                try
+                {
+                    assembly = Assembly.LoadFile(path);
+
+                    textBox.Text += "File  " + path + "  -  loaded" + Environment.NewLine + Environment.NewLine;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                textBox.Text += "Info of assembly:     " + assembly.FullName + Environment.NewLine + Environment.NewLine;
+
+                Type[] types = assembly.GetTypes();
+
+                foreach (Type type in types)
+                {
+                    textBox.Text += "Type:  " + type.FullName + Environment.NewLine;
+                    var attributes = type.GetCustomAttributes();
+                    if (attributes != null)
+                    {
+                        foreach (var attribute in attributes)
+                        {
+                            string methStr = "Type attribute: " + attribute.GetType().FullName + "\n";
+                            textBox.Text += methStr + Environment.NewLine;
+                        }
+                    }
+
+                    if (checkBoxMethods.Checked)
+                    {
+                        var methods = type.GetMethods();
+                        if (methods != null)
+                        {
+                            foreach (var method in methods)
+                            {
+                                string methStr = "Method: " + method.Name + ", return type = " + method.ReturnType + "\n";
+                                var subAttributes = method.GetCustomAttributes();
+                                if (subAttributes != null)
+                                {
+                                    foreach (var attribute in subAttributes)
+                                    {
+                                        methStr = "Type attribute: " + attribute.GetType().FullName + "\n";
+                                        textBox.Text += methStr + Environment.NewLine;
+                                    }
+                                }
+                                textBox.Text += methStr + Environment.NewLine;
+                            }
+                        }
+                    }
+
+                    if (checkBoxFields.Checked)
+                    {
+                        var fields = type.GetFields();
+                        if (fields != null)
+                        {
+                            foreach (var field in fields)
+                            {
+                                string methStr = "Field: " + field.Name + ", type = " + field.FieldType + "\n";
+                                var subAttributes = field.GetCustomAttributes();
+                                if (subAttributes != null)
+                                {
+                                    foreach (var attribute in subAttributes)
+                                    {
+                                        methStr = "Type attribute: " + attribute.GetType().FullName + "\n";
+                                        textBox.Text += methStr + Environment.NewLine;
+                                    }
+                                }
+                                textBox.Text += methStr + Environment.NewLine;
+                            }
+                        }
+                    }
+
+                    if (checkBoxProperties.Checked)
+                    {
+                        var properties = type.GetProperties();
+                        if (properties != null)
+                        {
+                            foreach (var property in properties)
+                            {
+                                string methStr = "Property: " + property.Name + ", type = " + property.PropertyType + "\n";
+                                var subAttributes = property.GetCustomAttributes();
+                                if (subAttributes != null)
+                                {
+                                    foreach (var attribute in subAttributes)
+                                    {
+                                        methStr = "Type attribute: " + attribute.GetType().FullName + "\n";
+                                        textBox.Text += methStr + Environment.NewLine;
+                                    }
+                                }
+                                textBox.Text += methStr + Environment.NewLine;
+                            }
+                        }
+                    }
+
+                    textBox.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
